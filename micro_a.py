acea9f0dfa44ef40e1c392c061612d8095dc46d8
ACSadighi <108032145+ACSadighi@users.noreply.github.com>
2024-05-20 13:49:13 -0700

    Add files via upload

---
+++ micro_a.py
@@ -0,0 +1,17 @@
+import os
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+while True:
+    plant = socket.recv_string()
+    d = os.getcwd() + '/Cs361_PlantStore'
+    images = os.listdir(d)
+    plant_image = plant + '.jpg'
+    path = 'Image not found'
+    if plant_image in images:
+        path = d + '/' + plant_image
+    print('Microservice responding...\n')
+    socket.send_string(path)
